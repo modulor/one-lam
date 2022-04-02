aedc46b1065a817f35b6341a2a100cd441d6d57f
Mario Lopez <modulor@hotmail.es>
2022-04-02 12:32:43 -0600

    Include font

--- application/views/login/login_view.php
+++ application/views/login/login_view.php
@@ -7,12 +7,27 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>ONE LAM CONFERENCE - POWER DEL CARMEN</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" href="<?php echo base_url('assets/css/fonts.css') ?>">
   <link rel="stylesheet" href="<?php echo base_url('assets/css/login.css') ?>">
 </head>
 
 <body>
   <main>
-    <h1>Login</h1>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4 offset-md-4">
+          <div class="card">
+            <div class="card-body">
+              <div class="event-title">
+                <h1>ONE LAM</h1>
+                <h2>CONFERENCE</h2>
+                <h3>POWER DEL CARMEN</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </main>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
