ede56cea131053afd4117065aa303e9a0e20e00a
Mario Lopez <modulor@hotmail.es>
2022-04-06 11:10:08 -0500

    WIP Landing: Menu en modo pantalla completa

--- application/views/landing/landing_view.php
+++ application/views/landing/landing_view.php
@@ -6,15 +6,56 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>ONE LAM CONFERENCE - POWER DEL CARMEN</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="<?php echo base_url('assets/css/fonts.css') ?>">
   <link rel="stylesheet" href="<?php echo base_url('assets/css/general.css') ?>">
-  <link rel="stylesheet" href="<?php echo base_url('assets/css/style.css') ?>">
   <link rel="stylesheet" href="<?php echo base_url('assets/css/landing.css') ?>">
 </head>
 
 <body>
+  <div id="myNav" class="overlay">
 
+    <!-- Button to close the overlay navigation -->
+    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+
+    <!-- Overlay content -->
+    <div class="overlay-content">
+      <a href="#">About</a>
+      <a href="#">Services</a>
+      <a href="#">Clients</a>
+      <a href="#">Contact</a>
+    </div>
+
+  </div>
+  <nav class="navbar navbar-expand-md navbar-light fixed-top bg-light">
+    <div class="container py-2">
+      <div class="col-4">
+        <button onclick="openNav()" type="button" class="btn btn-link">
+          <i class="fa-solid fa-bars"></i>
+        </button>
+      </div>
+      <div class="col-4 text-center">LOGO</div>
+      <div class="col-4 text-end">AVATAR</div>
+    </div>
+  </nav>
+  <main class="container">
+    <div class="row">
+      <div class="col">
+        <p>Hello</p>
+      </div>
+    </div>
+  </main>
+  <script>
+    function openNav() {
+      document.getElementById("myNav").style.width = "100%";
+    }
+
+    /* Close when someone clicks on the "x" symbol inside the overlay */
+    function closeNav() {
+      document.getElementById("myNav").style.width = "0%";
+    }
+  </script>
 </body>
 
 </html>
