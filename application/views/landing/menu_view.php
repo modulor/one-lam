e129cbae4761c86bfe63d77396bdb13aca9f4a4a
Mario Lopez <modulor@hotmail.es>
2022-04-06 20:27:54 -0500

    Menu

--- application/views/landing/menu_view.php
+++ application/views/landing/menu_view.php
@@ -1,9 +1,26 @@
 <div id="menu-overlay" class="menu-overlay">
   <a href="javascript:void(0)" class="closebtn" onclick="closeMenu()">&times;</a>
   <div class="menu-overlay-content">
-    <a href="#">Acerca de</a>
-    <a href="#">Services</a>
-    <a href="#">Clients</a>
-    <a href="<?php echo base_url('logout') ?>">Salir</a>
+    <button class="btn btn-link btn-link-menu" type="button" data-bs-toggle="collapse" data-bs-target="#schedule-collapse" aria-expanded="false" aria-controls="schedule-collapse">
+      <i class="fa-solid fa-plus"></i> Agenda
+    </button>
+    <div class="collapse" id="schedule-collapse">
+      <div class="p-4">
+        <a href="<?php echo base_url('schedule') ?>" class="link-submenu">
+          <img src="<?php echo base_url('assets/images/arrow-right.png') ?>" alt="arrow right"> 25 de abril
+        </a>
+        <a href="<?php echo base_url('schedule') ?>" class="link-submenu">
+          <img src="<?php echo base_url('assets/images/arrow-right.png') ?>" alt="arrow right"> 26 de abril
+        </a>
+        <a href="<?php echo base_url('schedule') ?>" class="link-submenu">
+          <img src="<?php echo base_url('assets/images/arrow-right.png') ?>" alt="arrow right"> 27 de abril
+        </a>
+        <a href="<?php echo base_url('schedule') ?>" class="link-submenu">
+          <img src="<?php echo base_url('assets/images/arrow-right.png') ?>" alt="arrow right"> 28 de abril
+        </a>
+      </div>
+    </div>
+    <a href="<?php echo base_url('experiences') ?>">Experencia del día</a>
+    <a href="<?php echo base_url('more') ?>">Más Info</a>
   </div>
 </div>
