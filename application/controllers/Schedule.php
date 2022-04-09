cd7ba249a3f71da3f6170036f3c38ca53413726f
Mario Lopez <modulor@hotmail.es>
2022-04-09 15:41:08 -0500

    Agenda

---
+++ application/controllers/Schedule.php
@@ -0,0 +1,21 @@
+<?php
+
+class Schedule extends CI_Controller
+{
+  function __construct()
+  {
+    parent::__construct();
+
+    if (!$this->session->userdata('login'))
+      redirect(base_url('login'));
+  }
+
+  public function index()
+  {
+    $data = array(
+      'view' => 'schedule/schedule_index_view',
+    );
+
+    $this->load->view('landing/landing_view', $data);
+  }
+}
---
+++ application/views/schedule/schedule_index_view.php
@@ -0,0 +1,116 @@
+<main class="container-fluid bg-landing pt-4">
+  <div class="row">
+    <div class="col-md-4 offset-md-4">
+      <div class="card">
+        <div class="card-body pt-2 schedule-card-body">
+          <h1>Agenda</h1>
+          <div class="row mb-2">
+            <div class="col-4">
+              <div class="schedule-bg-gray p-2 schedule-left-column text-center">
+                MORNING
+              </div>
+            </div>
+            <div class="col-8">
+              <div class="schedule-right-column schedule-bg-green p-2">
+                WE ARE ROOTED IN SPORTS
+              </div>
+            </div>
+          </div>
+          <div class="row mb-2">
+            <div class="col-4">
+              <div class="schedule-bg-gray p-2 schedule-left-column text-center">
+                AM
+              </div>
+            </div>
+            <div class="col-8">
+              <div class="schedule-right-column schedule-bg-yellow p-2 mb-1">
+                FREE BREAKFAST
+              </div>
+              <div class="schedule-right-column schedule-bg-gray p-2 mb-1">
+                WELCOME & OPENING WINNING IN LAM IN 2025
+              </div>
+              <div class="schedule-right-column schedule-bg-gray p-2">
+                WINNING IN LAM - CHECK IN WHERE DO WE STAND TODAY?
+              </div>
+            </div>
+          </div>
+          <div class="row mb-2">
+            <div class="col-4">
+              <div class="schedule-bg-gray p-2 schedule-left-column text-center">
+                LUNCH
+              </div>
+            </div>
+            <div class="col-8">
+              <div class="schedule-right-column schedule-bg-yellow p-2 mb-1">
+                FREE LUNCH
+              </div>
+            </div>
+          </div>
+          <div class="row mb-2">
+            <div class="col-4">
+              <div class="schedule-bg-gray p-2 schedule-left-column text-center">
+                PM
+              </div>
+            </div>
+            <div class="col-8">
+              <div class="schedule-right-column schedule-bg-pink p-2 mb-1">
+                WINNING THROUGH CULTURE<br>
+                HOW WILL WE EVOLVE A WINNING CULTURE BY LIVING OUR NEW VALUES?
+              </div>
+              <div class="schedule-right-column schedule-bg-pink p-2">
+                COFFE WITH SLT
+              </div>
+            </div>
+          </div>
+          <div class="row mb-5">
+            <div class="col-4">
+              <div class="schedule-bg-gray p-2 schedule-left-column text-center">
+                EVENING
+              </div>
+            </div>
+            <div class="col-8">
+              <div class="schedule-right-column schedule-bg-yellow p-2 mb-1">
+                CASUAL WELCOME DRINKS<br>
+                (POOL)
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-3">
+              <div class="d-grid gap-2">
+                <a href="<?php echo base_url('schedule') ?>" class="btn btn-dark">
+                  <span class="schedule-date">25</span><br>
+                  Abril
+                </a>
+              </div>
+            </div>
+            <div class="col-3">
+              <div class="d-grid gap-2">
+                <a href="<?php echo base_url('schedule') ?>" class="btn btn-light btn-border-black">
+                  <span class="schedule-date">26</span><br>
+                  Abril
+                </a>
+              </div>
+            </div>
+            <div class="col-3">
+              <div class="d-grid gap-2">
+                <a href="<?php echo base_url('schedule') ?>" class="btn btn-light btn-border-black">
+                  <span class="schedule-date">27</span><br>
+                  Abril
+                </a>
+              </div>
+            </div>
+            <div class="col-3">
+              <div class="d-grid gap-2">
+                <a href="<?php echo base_url('schedule') ?>" class="btn btn-light btn-border-black">
+                  <span class="schedule-date">28</span><br>
+                  Abril
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
