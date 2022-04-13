4370fda413169083246b226f4d421ad86a86202c
Mario Lopez <modulor@hotmail.es>
2022-04-13 12:01:49 -0500

    More: agregar icono de spotify

--- application/views/more/more_index_view.php
+++ application/views/more/more_index_view.php
@@ -1,7 +1,7 @@
 <main class="container-fluid bg-more pt-4">
   <div class="row">
     <div class="col-12 col-md-8 offset-md-2">
-      <div class="card">
+      <div class="card mb-4">
         <div class="card-body pt-2">
           <h1 class="mb-4">+Info</h1>
           <div class="d-block d-sm-none mb-4">
@@ -11,14 +11,17 @@
             <img src="<?php echo base_url('assets/images/map-desktop.jpeg') ?>" alt="map" class="img-fluid">
           </div>
           <div class="row">
-            <div class="col-3 text-center">
+            <div class="col-6 text-center">
               <a href="<?php echo base_url('more') ?>">
                 <img src="<?php echo base_url('assets/images/icon-whatsapp.png') ?>" alt="whatsapp" width="64">
               </a>
+              <p><small>Get personalized attention through Whatsapp</small></p>
             </div>
-            <div class="col-9">
-              <p>Recibe atención personalizada<br>
-                a través de Whatsapp</p>
+            <div class="col-6 text-center">
+              <a href="<?php echo base_url('more') ?>">
+                <img src="<?php echo base_url('assets/images/icon-spotify.png') ?>" alt="whatsapp" width="64">
+              </a>
+              <p><small>Win win<br>Rock You!</small></p>
             </div>
           </div>
         </div>
