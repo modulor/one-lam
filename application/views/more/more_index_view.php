7c796d60aa6cd1345482ee552b80ef4d640f3cbb
Mario Lopez <modulor@hotmail.es>
2022-04-13 12:19:49 -0500

    More: Modal para el mapa

--- application/views/more/more_index_view.php
+++ application/views/more/more_index_view.php
@@ -5,10 +5,10 @@
         <div class="card-body pt-2">
           <h1 class="mb-4">+Info</h1>
           <div class="d-block d-sm-none mb-4">
-            <img src="<?php echo base_url('assets/images/map-mobile.jpeg') ?>" alt="map" class="img-fluid">
+            <img data-bs-toggle="modal" data-bs-target="#mapModal" src="<?php echo base_url('assets/images/map-mobile.jpeg') ?>" alt="map" class="img-fluid">
           </div>
           <div class="d-none d-sm-block mb-4">
-            <img src="<?php echo base_url('assets/images/map-desktop.jpeg') ?>" alt="map" class="img-fluid">
+            <img data-bs-toggle="modal" data-bs-target="#mapModal" src="<?php echo base_url('assets/images/map-desktop.jpeg') ?>" alt="map" class="img-fluid">
           </div>
           <div class="row">
             <div class="col-6 text-center">
@@ -29,3 +29,20 @@
     </div>
   </div>
 </main>
+<div class="modal fade" id="mapModal" tabindex="-1" aria-labelledby="mapModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered modal-xl">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <div class="d-block d-sm-none mb-4">
+          <img src="<?php echo base_url('assets/images/map-mobile.jpeg') ?>" alt="map" class="img-fluid">
+        </div>
+        <div class="d-none d-sm-block mb-4">
+          <img src="<?php echo base_url('assets/images/map-desktop.jpeg') ?>" alt="map" class="img-fluid">
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
