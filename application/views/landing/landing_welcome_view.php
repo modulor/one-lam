e176913d7fbf491a407cf835cad4fda68a686bda
Mario Lopez <modulor@hotmail.es>
2022-04-14 17:44:59 -0500

    Landing Welcome: arreglar ui para desktop

--- application/views/landing/landing_welcome_view.php
+++ application/views/landing/landing_welcome_view.php
@@ -1,22 +1,32 @@
-<main class="container pt-4">
+<main class="container-fluid pt-4 bg-landing-welcome">
   <div class="row">
-    <div class="col-md-4 offset-md-4">
-      <h1>
-        Welcome back, <?php echo $this->session->userdata('alias') ?>, victory is right ahead!
-      </h1>
-      <div class="avatar-preview">
-        <img class="user-avatar-preview-img" src="<?php echo base_url("assets/images/avatar/background/background-400px-{$this->session->userdata('avatar')->background}.jpg") ?>" alt="background selected">
-        <img class="user-avatar-preview-img" src="<?php echo base_url("assets/images/avatar/body/body-400px-{$this->session->userdata('avatar')->body}.png") ?>" alt="avatar body">
-        <img class="user-avatar-preview-img" src="<?php echo base_url("assets/images/avatar/eyes/eyes-400px-{$this->session->userdata('avatar')->eyes}.png") ?>" alt="eyes selected">
-        <img class="user-avatar-preview-img" src="<?php echo base_url("assets/images/avatar/accessory/accessory-400px-{$this->session->userdata('avatar')->accessory}.png") ?>" alt="eyes saccessory">
-        <img class="user-avatar-preview-img" src="<?php echo base_url("assets/images/avatar/badge/badge-400px-{$this->session->userdata('avatar')->badge}.png") ?>" alt="badge selected">
-      </div>
-      <div class="row">
-        <div class="col text-end">
-          <img src="<?php echo base_url('assets/images/arrow-right.png') ?>" alt="arrow right">
-          <a href="<?php echo base_url('landing') ?>" class="btn btn-link">
-            Next
-          </a>
+    <div class="col-md-8 offset-md-2">
+      <div class="card">
+        <div class="card-body">
+          <div class="row">
+            <div class="col-md-6">
+              <h1 class="landing-welcome-title">
+                Welcome back, <?php echo $this->session->userdata('alias') ?>, victory is right ahead!
+              </h1>
+            </div>
+            <div class="col-md-6">
+              <div class="avatar-preview">
+                <img class="user-avatar-preview-img" src="<?php echo base_url("assets/images/avatar/background/background-400px-{$this->session->userdata('avatar')->background}.jpg") ?>" alt="background selected">
+                <img class="user-avatar-preview-img" src="<?php echo base_url("assets/images/avatar/body/body-400px-{$this->session->userdata('avatar')->body}.png") ?>" alt="avatar body">
+                <img class="user-avatar-preview-img" src="<?php echo base_url("assets/images/avatar/eyes/eyes-400px-{$this->session->userdata('avatar')->eyes}.png") ?>" alt="eyes selected">
+                <img class="user-avatar-preview-img" src="<?php echo base_url("assets/images/avatar/accessory/accessory-400px-{$this->session->userdata('avatar')->accessory}.png") ?>" alt="eyes saccessory">
+                <img class="user-avatar-preview-img" src="<?php echo base_url("assets/images/avatar/badge/badge-400px-{$this->session->userdata('avatar')->badge}.png") ?>" alt="badge selected">
+              </div>
+              <div class="row">
+                <div class="col text-end">
+                  <img src="<?php echo base_url('assets/images/arrow-right.png') ?>" alt="arrow right">
+                  <a href="<?php echo base_url('landing') ?>" class="btn btn-link">
+                    Next
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
