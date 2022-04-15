96783b8e9a144ac5a5531094938fdffbe20376ce
Mario Lopez <modulor@hotmail.es>
2022-04-15 11:05:25 -0500

    Landing y Menu

--- application/views/landing/menu_view.php
+++ application/views/landing/menu_view.php
@@ -1,9 +1,11 @@
 <div id="menu-overlay" class="menu-overlay">
   <a href="javascript:void(0)" class="closebtn" onclick="closeMenu()">&times;</a>
   <div class="menu-overlay-content">
-    <button class="btn btn-link btn-link-menu" type="button" data-bs-toggle="collapse" data-bs-target="#schedule-collapse" aria-expanded="false" aria-controls="schedule-collapse">
-      <i class="fa-solid fa-plus"></i> Winners Agenda
-    </button>
+    <div class="mb-1">
+      <button class="btn btn-link btn-link-menu" type="button" data-bs-toggle="collapse" data-bs-target="#schedule-collapse" aria-expanded="false" aria-controls="schedule-collapse">
+        <i class="fa-solid fa-plus"></i> Winners Agenda
+      </button>
+    </div>
     <div class="collapse" id="schedule-collapse">
       <div class="p-4">
         <a href="<?php echo base_url('schedule/day/25') ?>" class="link-submenu">
@@ -20,9 +22,11 @@
         </a>
       </div>
     </div>
-    <button class="btn btn-link btn-link-menu" type="button" data-bs-toggle="collapse" data-bs-target="#experience-collapse" aria-expanded="false" aria-controls="experience-collapse">
-      <i class="fa-solid fa-plus"></i> Daily Experience
-    </button>
+    <div class="mb-1">
+      <button class="btn btn-link btn-link-menu" type="button" data-bs-toggle="collapse" data-bs-target="#experience-collapse" aria-expanded="false" aria-controls="experience-collapse">
+        <i class="fa-solid fa-plus"></i> Daily Experience
+      </button>
+    </div>
     <div class="collapse" id="experience-collapse">
       <div class="p-4">
         <a href="<?php echo base_url('experiences/view/sports/1') ?>" class="link-submenu">
@@ -36,7 +40,9 @@
         </a>
       </div>
     </div>
-    <a href="<?php echo base_url('more') ?>">More</a>
+    <div class="mb-1">
+      <a href="<?php echo base_url('more') ?>">More</a>
+    </div>
     <div class="extra-material-area text-end">
       <a href="<?php echo base_url('extras') ?>" class="link-extra-material">
         <img src="<?php echo base_url('assets/images/arrow-right.png') ?>" alt="arrow right"> Download<br>Content
