fbdbfa4274a56e0ab6ee74895f4170029953e5b8
Mario Lopez <modulor@hotmail.es>
2022-04-18 08:57:10 -0500

    wip

--- application/views/more/more_index_view.php
+++ application/views/more/more_index_view.php
@@ -1,27 +1,29 @@
-<main class="container-fluid bg-more pt-4">
-  <div class="row">
-    <div class="col-12 col-md-8 offset-md-2">
-      <div class="card mb-4">
-        <div class="card-body pt-2">
-          <h1 class="mb-4">+Info</h1>
-          <div class="d-block d-sm-none mb-4">
-            <img data-bs-toggle="modal" data-bs-target="#mapModal" src="<?php echo base_url('assets/images/map-mobile.jpeg') ?>" alt="map" class="img-fluid">
-          </div>
-          <div class="d-none d-sm-block mb-4">
-            <img data-bs-toggle="modal" data-bs-target="#mapModal" src="<?php echo base_url('assets/images/map-desktop.jpeg') ?>" alt="map" class="img-fluid">
-          </div>
-          <div class="row">
-            <div class="col-6 text-center">
-              <a href="<?php echo base_url('more') ?>">
-                <img src="<?php echo base_url('assets/images/icon-whatsapp.png') ?>" alt="whatsapp" width="64">
-              </a>
-              <p><small>Get personalized attention through Whatsapp</small></p>
+<main class="bg-more pt-4">
+  <div class="container">
+    <div class="row">
+      <div class="col-12 col-md-7">
+        <div class="card mb-4">
+          <div class="card-body pt-2">
+            <h1 class="mb-4">More</h1>
+            <div class="d-block d-sm-none mb-4">
+              <img data-bs-toggle="modal" data-bs-target="#mapModal" src="<?php echo base_url('assets/images/map-mobile.jpeg') ?>" alt="map" class="img-fluid">
             </div>
-            <div class="col-6 text-center">
-              <a href="<?php echo base_url('more') ?>">
-                <img src="<?php echo base_url('assets/images/icon-spotify.png') ?>" alt="whatsapp" width="64">
-              </a>
-              <p><small>Win win<br>Rock You!</small></p>
+            <div class="d-none d-sm-block mb-4">
+              <img data-bs-toggle="modal" data-bs-target="#mapModal" src="<?php echo base_url('assets/images/map-desktop.jpeg') ?>" alt="map" class="img-fluid">
+            </div>
+            <div class="row">
+              <div class="col-6 text-center">
+                <a href="<?php echo base_url('more') ?>">
+                  <img src="<?php echo base_url('assets/images/icon-whatsapp.png') ?>" alt="whatsapp" width="64">
+                </a>
+                <p><small>Get personalized attention through Whatsapp</small></p>
+              </div>
+              <div class="col-6 text-center">
+                <a href="<?php echo base_url('more') ?>">
+                  <img src="<?php echo base_url('assets/images/icon-spotify.png') ?>" alt="whatsapp" width="64">
+                </a>
+                <p><small>Win win<br>Rock You!</small></p>
+              </div>
             </div>
           </div>
         </div>
