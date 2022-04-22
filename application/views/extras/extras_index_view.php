2f8dea5859195d53628d3a2faa53b4baa03cf72e
Mario Lopez <modulor@hotmail.es>
2022-04-22 16:09:26 -0500

    Extras: corregir padding para que se vea mejor la forma rosa

--- application/views/extras/extras_index_view.php
+++ application/views/extras/extras_index_view.php
@@ -1,14 +1,14 @@
 <main class="container-fluid bg-extras pt-4">
   <div class="row">
-    <div class="col-md-4 offset-md-4">
+    <div class="col-md-8">
       <div class="card">
         <div class="card-body pt-5 extras-card-body">
           <div class="extras-photos mb-4">
             <div class="row">
-              <div class="col-3">
+              <div class="col-3 col-md-2">
                 <img src="<?php echo base_url('assets/images/arrow-right.png') ?>" alt="arrow right" class="me-4">
               </div>
-              <div class="col-9">
+              <div class="col-9 col-md-10">
                 <a href="<?php echo base_url('extras') ?>" class="extras-link">
                   Photo Gallery
                 </a>
@@ -17,10 +17,10 @@
           </div>
           <div class="extras-archives mb-4">
             <div class="row">
-              <div class="col-3">
+              <div class="col-3 col-md-2">
                 <img src="<?php echo base_url('assets/images/arrow-right.png') ?>" alt="arrow right" class="me-4">
               </div>
-              <div class="col-9">
+              <div class="col-9 col-md-10">
                 <a href="<?php echo base_url('extras') ?>" class="extras-link">
                   Files
                 </a>
