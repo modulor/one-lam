4bb7e80907ffe9bfa4ca15ef2ac7e1b782266448
Mario Lopez <modulor@hotmail.es>
2022-04-25 12:02:29 -0500

    Agregar un div con scroll-y para las fotos

--- application/views/extras/extras_gallery_day_24_view.php
+++ application/views/extras/extras_gallery_day_24_view.php
@@ -1,124 +1,128 @@
-<div class="row">
-  <div class="col-4 col-md-3 mb-2">
-    <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_1615.jpeg')">
-      <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_1615.jpeg') ?>" class="img-fluid" alt="gallery">
-    </a>
-  </div>
-  <div class="col-4 col-md-3 mb-2">
-    <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_1629.jpeg')">
-      <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_1629.jpeg') ?>" class="img-fluid" alt="photo"></a>
-  </div>
-  <div class="col-4 col-md-3 mb-2">
-    <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_1638.jpeg')">
-      <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_1638.jpeg') ?>" class="img-fluid" alt="photo"></a>
-  </div>
-  <div class="col-4 col-md-3 mb-2">
-    <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_1651.jpeg')">
-      <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_1651.jpeg') ?>" class="img-fluid" alt="photo"></a>
-  </div>
-  <div class="col-4 col-md-3 mb-2">
-    <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_1655.jpeg')">
-      <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_1655.jpeg') ?>" class="img-fluid" alt="photo"></a>
-  </div>
-  <div class="col-4 col-md-3 mb-2">
-    <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_1667.jpeg')">
-      <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_1667.jpeg') ?>" class="img-fluid" alt="photo"></a>
-  </div>
-  <div class="col-4 col-md-3 mb-2">
-    <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_1672.jpeg')">
-      <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_1672.jpeg') ?>" class="img-fluid" alt="photo"></a>
-  </div>
-  <div class="col-4 col-md-3 mb-2">
-    <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_1683.jpeg')">
-      <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_1683.jpeg') ?>" class="img-fluid" alt="photo"></a>
-  </div>
-  <div class="col-4 col-md-3 mb-2">
-    <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_1685.jpeg')">
-      <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_1685.jpeg') ?>" class="img-fluid" alt="photo"></a>
-  </div>
-  <div class="col-4 col-md-3 mb-2">
-    <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_1695.jpeg')">
-      <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_1695.jpeg') ?>" class="img-fluid" alt="photo"></a>
-  </div>
-  <div class="col-4 col-md-3 mb-2">
-    <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_1724.jpeg')">
-      <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_1724.jpeg') ?>" class="img-fluid" alt="photo"></a>
-  </div>
-  <div class="col-4 col-md-3 mb-2">
-    <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_1746.jpeg')">
-      <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_1746.jpeg') ?>" class="img-fluid" alt="photo"></a>
-  </div>
-  <div class="col-4 col-md-3 mb-2">
-    <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_1757.jpeg')">
-      <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_1757.jpeg') ?>" class="img-fluid" alt="photo"></a>
-  </div>
-  <div class="col-4 col-md-3 mb-2">
-    <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_1781.jpeg')">
-      <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_1781.jpeg') ?>" class="img-fluid" alt="photo"></a>
-  </div>
-  <div class="col-4 col-md-3 mb-2">
-    <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_1788.jpeg')">
-      <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_1788.jpeg') ?>" class="img-fluid" alt="photo"></a>
-  </div>
-  <div class="col-4 col-md-3 mb-2">
-    <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_8049.jpeg')">
-      <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_8049.jpeg') ?>" class="img-fluid" alt="photo"></a>
-  </div>
-  <div class="col-4 col-md-3 mb-2">
-    <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_8122.jpeg')">
-      <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_8122.jpeg') ?>" class="img-fluid" alt="photo"></a>
-  </div>
-  <div class="col-4 col-md-3 mb-2">
-    <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_8126.jpeg')">
-      <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_8126.jpeg') ?>" class="img-fluid" alt="photo"></a>
-  </div>
-  <div class="col-4 col-md-3 mb-2">
-    <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_8136.jpeg')">
-      <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_8136.jpeg') ?>" class="img-fluid" alt="photo"></a>
-  </div>
-  <div class="col-4 col-md-3 mb-2">
-    <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_8143.jpeg')">
-      <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_8143.jpeg') ?>" class="img-fluid" alt="photo"></a>
-  </div>
-  <div class="col-4 col-md-3 mb-2">
-    <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_8153.jpeg')">
-      <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_8153.jpeg') ?>" class="img-fluid" alt="photo"></a>
-  </div>
-  <div class="col-4 col-md-3 mb-2">
-    <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_8154.jpeg')">
-      <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_8154.jpeg') ?>" class="img-fluid" alt="photo"></a>
-  </div>
-  <div class="col-4 col-md-3 mb-2">
-    <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_8163.jpeg')">
-      <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_8163.jpeg') ?>" class="img-fluid" alt="photo"></a>
-  </div>
-  <div class="col-4 col-md-3 mb-2">
-    <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_8192.jpeg')">
-      <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_8192.jpeg') ?>" class="img-fluid" alt="photo"></a>
-  </div>
-  <div class="col-4 col-md-3 mb-2">
-    <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_8200.jpeg')">
-      <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_8200.jpeg') ?>" class="img-fluid" alt="photo"></a>
-  </div>
-  <div class="col-4 col-md-3 mb-2">
-    <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_8204.jpeg')">
-      <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_8204.jpeg') ?>" class="img-fluid" alt="photo"></a>
-  </div>
-  <div class="col-4 col-md-3 mb-2">
-    <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_8217.jpeg')">
-      <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_8217.jpeg') ?>" class="img-fluid" alt="photo"></a>
-  </div>
-  <div class="col-4 col-md-3 mb-2">
-    <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_8233.jpeg')">
-      <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_8233.jpeg') ?>" class="img-fluid" alt="photo"></a>
-  </div>
-  <div class="col-4 col-md-3 mb-2">
-    <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_8236.jpeg')">
-      <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_8236.jpeg') ?>" class="img-fluid" alt="photo"></a>
-  </div>
-  <div class="col-4 col-md-3 mb-2">
-    <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_8245.jpeg')">
-      <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_8245.jpeg') ?>" class="img-fluid" alt="photo"></a>
+<div class="gallery-area">
+  <div class="container">
+    <div class="row">
+      <div class="col-4 col-md-3 mb-2">
+        <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_1615.jpeg')">
+          <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_1615.jpeg') ?>" class="img-fluid" alt="gallery">
+        </a>
+      </div>
+      <div class="col-4 col-md-3 mb-2">
+        <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_1629.jpeg')">
+          <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_1629.jpeg') ?>" class="img-fluid" alt="photo"></a>
+      </div>
+      <div class="col-4 col-md-3 mb-2">
+        <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_1638.jpeg')">
+          <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_1638.jpeg') ?>" class="img-fluid" alt="photo"></a>
+      </div>
+      <div class="col-4 col-md-3 mb-2">
+        <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_1651.jpeg')">
+          <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_1651.jpeg') ?>" class="img-fluid" alt="photo"></a>
+      </div>
+      <div class="col-4 col-md-3 mb-2">
+        <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_1655.jpeg')">
+          <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_1655.jpeg') ?>" class="img-fluid" alt="photo"></a>
+      </div>
+      <div class="col-4 col-md-3 mb-2">
+        <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_1667.jpeg')">
+          <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_1667.jpeg') ?>" class="img-fluid" alt="photo"></a>
+      </div>
+      <div class="col-4 col-md-3 mb-2">
+        <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_1672.jpeg')">
+          <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_1672.jpeg') ?>" class="img-fluid" alt="photo"></a>
+      </div>
+      <div class="col-4 col-md-3 mb-2">
+        <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_1683.jpeg')">
+          <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_1683.jpeg') ?>" class="img-fluid" alt="photo"></a>
+      </div>
+      <div class="col-4 col-md-3 mb-2">
+        <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_1685.jpeg')">
+          <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_1685.jpeg') ?>" class="img-fluid" alt="photo"></a>
+      </div>
+      <div class="col-4 col-md-3 mb-2">
+        <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_1695.jpeg')">
+          <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_1695.jpeg') ?>" class="img-fluid" alt="photo"></a>
+      </div>
+      <div class="col-4 col-md-3 mb-2">
+        <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_1724.jpeg')">
+          <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_1724.jpeg') ?>" class="img-fluid" alt="photo"></a>
+      </div>
+      <div class="col-4 col-md-3 mb-2">
+        <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_1746.jpeg')">
+          <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_1746.jpeg') ?>" class="img-fluid" alt="photo"></a>
+      </div>
+      <div class="col-4 col-md-3 mb-2">
+        <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_1757.jpeg')">
+          <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_1757.jpeg') ?>" class="img-fluid" alt="photo"></a>
+      </div>
+      <div class="col-4 col-md-3 mb-2">
+        <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_1781.jpeg')">
+          <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_1781.jpeg') ?>" class="img-fluid" alt="photo"></a>
+      </div>
+      <div class="col-4 col-md-3 mb-2">
+        <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_1788.jpeg')">
+          <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_1788.jpeg') ?>" class="img-fluid" alt="photo"></a>
+      </div>
+      <div class="col-4 col-md-3 mb-2">
+        <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_8049.jpeg')">
+          <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_8049.jpeg') ?>" class="img-fluid" alt="photo"></a>
+      </div>
+      <div class="col-4 col-md-3 mb-2">
+        <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_8122.jpeg')">
+          <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_8122.jpeg') ?>" class="img-fluid" alt="photo"></a>
+      </div>
+      <div class="col-4 col-md-3 mb-2">
+        <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_8126.jpeg')">
+          <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_8126.jpeg') ?>" class="img-fluid" alt="photo"></a>
+      </div>
+      <div class="col-4 col-md-3 mb-2">
+        <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_8136.jpeg')">
+          <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_8136.jpeg') ?>" class="img-fluid" alt="photo"></a>
+      </div>
+      <div class="col-4 col-md-3 mb-2">
+        <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_8143.jpeg')">
+          <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_8143.jpeg') ?>" class="img-fluid" alt="photo"></a>
+      </div>
+      <div class="col-4 col-md-3 mb-2">
+        <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_8153.jpeg')">
+          <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_8153.jpeg') ?>" class="img-fluid" alt="photo"></a>
+      </div>
+      <div class="col-4 col-md-3 mb-2">
+        <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_8154.jpeg')">
+          <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_8154.jpeg') ?>" class="img-fluid" alt="photo"></a>
+      </div>
+      <div class="col-4 col-md-3 mb-2">
+        <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_8163.jpeg')">
+          <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_8163.jpeg') ?>" class="img-fluid" alt="photo"></a>
+      </div>
+      <div class="col-4 col-md-3 mb-2">
+        <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_8192.jpeg')">
+          <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_8192.jpeg') ?>" class="img-fluid" alt="photo"></a>
+      </div>
+      <div class="col-4 col-md-3 mb-2">
+        <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_8200.jpeg')">
+          <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_8200.jpeg') ?>" class="img-fluid" alt="photo"></a>
+      </div>
+      <div class="col-4 col-md-3 mb-2">
+        <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_8204.jpeg')">
+          <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_8204.jpeg') ?>" class="img-fluid" alt="photo"></a>
+      </div>
+      <div class="col-4 col-md-3 mb-2">
+        <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_8217.jpeg')">
+          <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_8217.jpeg') ?>" class="img-fluid" alt="photo"></a>
+      </div>
+      <div class="col-4 col-md-3 mb-2">
+        <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_8233.jpeg')">
+          <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_8233.jpeg') ?>" class="img-fluid" alt="photo"></a>
+      </div>
+      <div class="col-4 col-md-3 mb-2">
+        <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_8236.jpeg')">
+          <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_8236.jpeg') ?>" class="img-fluid" alt="photo"></a>
+      </div>
+      <div class="col-4 col-md-3 mb-2">
+        <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#photoModal" onclick="selectPhoto('apr-24/JZP_8245.jpeg')">
+          <img src="<?php echo base_url('assets/images/gallery/apr-24/JZP_8245.jpeg') ?>" class="img-fluid" alt="photo"></a>
+      </div>
+    </div>
   </div>
 </div>
 
